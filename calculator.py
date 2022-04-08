ba6c2603c05c053b062efe8dd7db74d1c856e80a
wangton-Raw <70154157+wangton-Raw@users.noreply.github.com>
2022-04-08 14:19:16 +0200

    Create an GUI introduction

---
+++ calculator.py
@@ -0,0 +1,20 @@
+
+# hello_psg.py
+
+import PySimpleGUI as sg
+
+layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+
+# Create the window
+window = sg.Window("Demo", layout, size = (500,300), resizable = True)
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
+
