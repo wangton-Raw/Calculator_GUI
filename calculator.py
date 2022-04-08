c54081896a8d6547e29a97aa7d24702f15a36f32
FjeldMats <FjeldMats@gmail.com>
2022-04-08 14:44:34 +0200

    change name

--- calculator.py
+++ calculator.py
@@ -11,7 +11,7 @@ layout = [[sg.Text('Enter two numbers')],
 
 
 # Create the window
-window = sg.Window("Demo", layout, size = (500,300), resizable = True)
+window = sg.Window("Demo2", layout, size = (500,300), resizable = True)
 
 while True:
     event, values = window.read()
